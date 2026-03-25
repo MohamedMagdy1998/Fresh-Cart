d4d09dfbcd78aba4daf5a83df5be11bc18d2c838
Mohamed Magdy <mohamed9898magdy@gmail.com>
2026-03-25 08:27:30 +0200

    initial commit Project Setup,Handling routing, Folder Structure, Design of pages

---
+++ src/app/Layout/Additions/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { FlowbiteService } from '../../../Shared/Services/Flowbite/Flowbite.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Layout/Pages/home/home.component';
+import { ProductsComponent } from './Layout/Pages/products/products.component';
+import { CartComponent } from './Layout/Pages/cart/cart.component';
+import { LogInComponent } from './Layout/Pages/log-in/log-in.component';
+import { RegisterComponent } from './Layout/Pages/register/register.component';
+import { BrandsComponent } from './Layout/Pages/brands/brands.component';
+import { CategoriesComponent } from './Layout/Pages/categories/categories.component';
+import { NotFoundComponent } from './Layout/Additions/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:"", redirectTo: "home", pathMatch: "full"},
+{path:"home", title: "Home" ,component: HomeComponent},
+{path:"products", title: "Products" ,component: ProductsComponent},
+{path:"cart", title: "Cart" ,component: CartComponent},
+{path:"login", title: "Login" ,component: LogInComponent},
+{path:"register", title: "Register" ,component: RegisterComponent},
+{path:"brands", title: "Brands" ,component: BrandsComponent},
+{path:"categories", title: "Categories" ,component: CategoriesComponent},
+{path:"**", title: "Not Found" ,component: NotFoundComponent}  
+
+
+
+
+
+
+
+
+
+];
